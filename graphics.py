c2476c9562598091d93de327b3f16d6786c06fee
Callum Alexander <46627336+CallumAlexander@users.noreply.github.com>
2019-02-19 19:59:13 +0000

    Add files via upload

---
+++ graphics.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# Property of Callum Alexander
+# Instagram - @cal.zander
+# Twitter - callum_alxndr
+"""
+@author: Callum
+"""
+
+import os
+import sys
+clear = lambda: os.system("cls")
+
+import time
+
+def Intro():
+    
+    clear()
+    
+    delay = 0.15
+    
+    print("   _____                       ")
+    time.sleep(delay)
+    print("  / ____|                      ")
+    time.sleep(delay)
+    print(" | (___   __ _ _   _ _ __ __ _ ")
+    time.sleep(delay)
+    print("  \___ \ / _` | | | | '__/ _` |")
+    time.sleep(delay)
+    print("  ____) | (_| | |_| | | | (_| |")
+    time.sleep(delay)
+    print(" |_____/ \__,_|\__,_|_|  \__,_|")
+    time.sleep(delay)
+    print("                               ")
+    time.sleep(1)
+    print(" Welcome traveller")
+    time.sleep(2)
+    print(" This is the official Cansat Saura User Interface and Ground Control xx")
+    time.sleep(3)
+    print(" ")
+    print(" This system is a command line interface")
+    print(" To view the list of commands available for this system, please input 'help'.")
+    time.sleep(3)
+    
+    
+    
+def Title():
+    print("   _____                       ")
+    print("  / ____|                      ")
+    print(" | (___   __ _ _   _ _ __ __ _ ")
+    print("  \___ \ / _` | | | | '__/ _` |")
+    print("  ____) | (_| | |_| | | | (_| |")
+    print(" |_____/ \__,_|\__,_|_|  \__,_|")
+    print("                               ")
+    
+
+    
+    
+    
