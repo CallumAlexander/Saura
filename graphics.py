a4ad281af820921f015dcc1972047b51a1ad1d69
Callum Alexander <46627336+CallumAlexander@users.noreply.github.com>
2019-02-20 20:57:37 +0000

    Add files via upload

--- graphics.py
+++ graphics.py
@@ -36,10 +36,7 @@ def Intro():
     time.sleep(2)
     print(" This is the official Cansat Saura User Interface and Ground Control xx")
     time.sleep(3)
-    print(" ")
-    print(" This system is a command line interface")
-    print(" To view the list of commands available for this system, please input 'help'.")
-    time.sleep(3)
+    
     
     
     
